aa8823206ffb963dee419ddf1fba51d244eee679
Nathan Phame <diyf2021@gmail.com>
2021-05-19 17:23:16 -0400

    Display winner name for team 1

--- public/main.js
+++ public/main.js
@@ -4,9 +4,24 @@ function updateTeamName(event) {
 }
 document.querySelector('.team1 input').addEventListener('input', updateTeamName)
 
+// const teamName = document.querySelectorAll('h2')
+// const inputElement = document.querySelectorAll('input')
+// function updateTeamName(event) {
+//   const currentInputValue = event.target.value
+//   inputElement.textContent = currentInputValue
+// }
+// teamName.forEach(element => element.addEventListener('input', updateTeamName))
+
+const y = document.querySelector('.team1 h2')
+const x = y.textContent
+
 let moveCounter = 0
 function updateScoreAddition(event) {
   moveCounter++
+  if (moveCounter > 21) {
+    window.alert(`You won ${x}`)
+    return
+  }
   const addScore = document.querySelector('.team1 h3')
   addScore.textContent = moveCounter
 }
@@ -14,6 +29,10 @@ document.querySelector('.add').addEventListener('click', updateScoreAddition)
 
 function updateScoreMinus(event) {
   moveCounter--
+  if (moveCounter < 0) {
+    window.alert('ayyyy thats negative no way jose')
+    return
+  }
   const subtractScore = document.querySelector('.team1 h3')
   subtractScore.textContent = moveCounter
 }
