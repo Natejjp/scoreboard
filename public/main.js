18b322354ffe1842ea87252c6fb1ab8a66333db8
Nathan Phame <diyf2021@gmail.com>
2021-05-19 14:00:33 -0400

    Can change team 1 name

--- public/main.js
+++ public/main.js
@@ -1,7 +1,23 @@
-function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+const inputElement = document.querySelector('input')
+const headerTwoElement = document.querySelector('h2')
+
+const updateTeamName = event => {
+  const currentInputValue = event.target.value
+  headerTwoElement.innerText = currentInputValue
 }
+
+inputElement.addEventListener('input', updateTeamName)
+
+const addition = document.querySelector('.add')
+let headerThreeElement = document.querySelector('h3')
+
+const addScore = eventTwo => {
+  const addButton = eventTwo.target
+  // const addButton = eventTwo.target
+  // headerThreeElement.value = 1
+}
+addition.addEventListener('click', addScore)
+
+function main() {}
 
 document.addEventListener('DOMContentLoaded', main)
